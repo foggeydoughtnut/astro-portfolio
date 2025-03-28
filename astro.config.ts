1f7f1f2009b006e3ffdd282ac8edd5d4c95cbfd4
Jeff Anderson <foggeydoughtnut@gmail.com>
2025-03-27 19:38:24 -0600

    Brought in an Icon rendering library

--- astro.config.ts
+++ astro.config.ts
@@ -4,9 +4,11 @@ import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import tailwindcss from '@tailwindcss/vite';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
+  integrations: [vue(), icon()],
 
   vite: {
     plugins: [tailwindcss()]
